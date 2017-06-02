a0061979e8eca2b9c8f99eb87cf0fc4b30cb6e15
Blake Barrett <bbarrett1@pandora.com>
2017-06-02 11:09:44 -0700

    Adding these files too

---
+++ Example/Pods/Target Support Files/Pods-CocoaPodTest_Example/Pods-CocoaPodTest_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_CocoaPodTest_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CocoaPodTest_ExampleVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-CocoaPodTest_Tests/Pods-CocoaPodTest_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_CocoaPodTest_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CocoaPodTest_TestsVersionString[];
+
